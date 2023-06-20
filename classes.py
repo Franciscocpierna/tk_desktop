effd973408481e40f493c703c4751351da1ace01
FRANCISCO CARLOS PIERNA <pierna_f@hotmail.com>
2023-06-20 17:53:03 -0300

    tela tkinter classe

--- classes.py
+++ classes.py
@@ -1,5 +1,6 @@
-import tkinter as tk
-class tela():
+from tkinter import *
+
+class tela(Frame):
     def __init__(self,**kwargs):
         super().__init__()
         for chave in kwargs:
@@ -10,9 +11,27 @@ class tela():
     
     def set_tela(self,v):
         self.x = v
-root= tk()
+janela= Tk()
+janela1 = Tk()
+print(help(Frame))
+#jam1=tela(janela)
+janela.configure(background='Gray')
+janela.configure(height= 2000)
+janela.configure(width=2000)
+janela1.mainloop()
+janela.mainloop()
 
 '''import Tkinter
 root = Tkinter.Tk()
 root.configure(background='black') 
-root.mainloop()'''
+root.mainloop()
+super().__init__()
+      self['height'] =150
+      self['width'] = 200
+      self['bd'] = 2
+      self['relief']= SOLID
+
+      label_nome = Label(self,text=texto)
+      text_nome = Entry(self)
+      label_nome.grid(row=0,column=0)
+      text_nome.grid(row=0, column=1)'''
