28074c7b25248f69a0929c41ea1445d2900ea3eb
FRANCISCO CARLOS PIERNA <pierna_f@hotmail.com>
2023-07-09 14:12:17 -0300

    tela/classe 09/07/2023

--- contasapagar/contaspagar.py
+++ contasapagar/contaspagar.py
@@ -1,9 +1,10 @@
 from tkinter import *
 from modulos.classes import * #montatela
-#from modulos.centralizatela import centralizacao
+
 
 
 #import awesometkinter as atk
+
 largura=0
 altura=0
 posx=0
@@ -11,66 +12,41 @@ posy=0
 X=0
 ler=""
 opcao=0
-tela=""
-'''codigo="inicio"
-nome=""
-endereco=""
-telefone=""
-tipo=""
-cpf=""
-cnpj=""
-'''
-'''def tela1(manutencao):
-    Label(manutencao, text="Codigo:").grid(row=1, column=0)
-    codigo = Entry(manutencao,width=40).grid(row=1, column=1)
-    Label(manutencao, text="Nome:").grid(row=2, column=0)
-    nome = Entry(manutencao,width=40).grid(row=2, column=1)
-    Label(manutencao, text="Endereço:").grid(row=3, column=0)
-    endereco= Entry(manutencao,width=40).grid(row=3, column=1)
-    Label(manutencao, text="Telefone:").grid(row=4, column=0)
-    telefone= Entry(manutencao,width=40).grid(row=4, column=1)
-    Label(manutencao, text="Tipo:").grid(row=5, column=0)
-    tipo = Entry(manutencao,width=40).grid(row=5, column=1)
-    Label(manutencao, text="Cpf:").grid(row=6, column=0)
-    cpf = Entry(manutencao,width=40).grid(row=6, column=1)
-    Label(manutencao, text="Cnpj:").grid(row=7, column=0)
-    cnpj = Entry(manutencao,width=40).grid(row=7, column=1)
-    ler=codigo.get()
-    return '''     
+
+
 def verconteudo():
      print (f'self.codigo.get = {tela.codigo.get()}') #{tela.codigo.get()}')
      
 def incluir_click():
      opcao=1
+     global tela  
      manutencao = Toplevel() # janela de nível superior
      tela = montatela(manutencao,janela1,opcao,posx,posy)
-     print (f'tela.codigo.get = {tela.codigo.get()}')
-
-     #tela1(manutencao) 
-     
      botao=Button(manutencao, text='contem', command = verconteudo)
-     botao.grid(row=20, column=1,sticky=N)
+     botao.grid(row=20, column=0,sticky=N)
      
 def cosulta_click():
      opcao=2
+     global tela
      manutencao = Toplevel() # janela de nível superior
      tela = montatela(manutencao,janela1,opcao,posx,posy)
-     #tela1(manutencao) 
-      
-     print(f'a opcao consulta aqui é tela.opcao = {tela.opcao}')
+     botao=Button(manutencao, text='contem', command = verconteudo)
+     botao.grid(row=20, column=0,sticky=E) 
 def alteracao_clik():
      opcao=3
+     global tela
      manutencao = Toplevel() # janela de nível superior
      tela = montatela(manutencao,janela1,opcao,posx,posy)
-     #tela1(manutencao) 
-     print(f'a opcao alteraçao aqui é {opcao}')
+     botao=Button(manutencao, text='contem', command = verconteudo)
+     botao.grid(row=20, column=0,sticky=N)
 def excluir_click(): 
      opcao=4
+     global tela
      manutencao = Toplevel() # janela de nível superior
      tela = montatela(manutencao,janela1,opcao,posx,posy)
-     #tela1(manutencao) 
-     print(f'a opcao Exclusao aqui é {opcao}')
-
+     botao=Button(manutencao, text='contem', command = verconteudo)
+     botao.grid(row=20, column=1,sticky=N)
+ 
 janela= Tk()
 janela.title("janela principal")
 janela.configure(background='Gray')
@@ -116,20 +92,3 @@ centro=centralizacao(janela1,largura, altura, posx, posy)
 janela1.geometry("%dx%d+%d+%d" % (centro.largura1, centro.altura1, centro.posx, centro.posy))
 janela1.mainloop()
 janela.mainloop()
-
-
-
-'''import Tkinter
-root = Tkinter.Tk()
-root.configure(background='black') 
-root.mainloop()
-super().__init__()
-      self['height'] =150
-      self['width'] = 200
-      self['bd'] = 2
-      self['relief']= SOLID
-
-      label_nome = Label(self,text=texto)
-      text_nome = Entry(self)
-      label_nome.grid(row=0,column=0)
-      text_nome.grid(row=0, column=1)'''
--- contasapagar/modulos/classes.py
+++ contasapagar/modulos/classes.py
@@ -11,17 +11,17 @@ class montatela():
               
          if self.opcao==1:
               manutencao1.title("Inclusão")
-              Label(manutencao1, text= 'Manutenção - Inclusão',font=('Arial', 20) ,fg="black",).grid(column=2,row=0,padx= 220, pady=30)
+              Label(manutencao1, text= 'Manutenção - Inclusão',font=('Arial', 20) ,fg="black",).grid(column=2,row=0,padx= 90, pady=30)
               
          elif self.opcao == 2:
              manutencao1.title("Consulta")
-             Label(manutencao1, text= 'Manutenção - Consulta',font=('Arial', 20) ,fg="black",).grid(column=2,row=0,padx= 220, pady=30)
+             Label(manutencao1, text= 'Manutenção - Consulta',font=('Arial', 20) ,fg="black",).grid(column=2,row=0,padx= 90, pady=30)
          elif self.opcao == 3:
               manutencao1.title("Ateração")    
-              Label(manutencao1, text= 'Manutenção - Alteração',font=('Arial', 20) ,fg="black",).grid(column=2,row=0,padx= 220, pady=30) 
+              Label(manutencao1, text= 'Manutenção - Alteração',font=('Arial', 20) ,fg="black",).grid(column=2,row=0,padx= 90, pady=30) 
          else:
               manutencao1.title("Exclusão")
-              Label(manutencao1, text= 'Manutenção - Exclusão',font=('Arial', 20) ,fg="black",).grid(column=2,row=0,padx= 220, pady=30)
+              Label(manutencao1, text= 'Manutenção - Exclusão',font=('Arial', 20) ,fg="black",).grid(column=2,row=0,padx= 90, pady=30)
           #janela1.configure(height= 400)
           #janela1.configure(width= 400) 
                   
@@ -30,34 +30,33 @@ class montatela():
          manutencao1.focus_force() #forçar foco
          manutencao1.grab_set()    # impede que click na janela principal sem fechar
          centralizatela = centralizacao(manutencao1,1200,650,self.posx,self.posy)
-         Label(manutencao1, text="Codigo:").grid(row=1, column=0)
-         self.codigo = Entry(manutencao1,width=40)
-         self.codigo.grid(row=1, column=1, sticky=N)
-         Label(manutencao1, text="Nome:").grid(row=2, column=0)
-         self.nome = Entry(manutencao1,width=40)
-         self.nome.grid(row=2, column=1,sticky=N)
-         Label(manutencao1, text="Endereço:").grid(row=3, column=0)
-         self.endereco= Entry(manutencao1,width=40)
-         self.endereco.grid(row=3, column=1,sticky=N)
-         Label(manutencao1, text="Telefone:").grid(row=4, column=0)
-         self.telefone= Entry(manutencao1,width=40)
-         self.telefone.grid(row=4, column=1,sticky=N)
-         Label(manutencao1, text="Tipo:").grid(row=5, column=0)
-         self.tipo = Entry(manutencao1,width=40)
-         self.tipo.grid(row=5, column=1,sticky=N)
-         Label(manutencao1, text="Cpf:").grid(row=6, column=0)
-         self.cpf = Entry(manutencao1,width=40)
-         self.cpf.grid(row=6, column=1,sticky=N)
-         Label(manutencao1, text="Cnpj:").grid(row=7, column=0)
-         self.cnpj = Entry(manutencao1,width=40)
-         self.cnpj.grid(row=7, column=1,sticky=N)
+         Label(manutencao1, text="Codigo:", font=('Arial', 15)).grid(row=1, column=0,sticky=W)
+         self.codigo = Entry(manutencao1,width=7)
+         self.codigo.grid(row=1, column=1, sticky=W)
+         Label(manutencao1, text="Nome:",font=('Arial', 15)).grid(row=2, column=0,sticky=W)
+         self.nome = Entry(manutencao1,width=50)
+         self.nome.grid(row=2, column=1,sticky=W)
+         Label(manutencao1, text="Endereço:",font=('Arial', 15)).grid(row=3, column=0,sticky=W)
+         self.endereco= Entry(manutencao1,width=50)
+         self.endereco.grid(row=3, column=1,sticky=W)
+         Label(manutencao1, text="Telefone:",font=('Arial', 15)).grid(row=4, column=0,sticky=W)
+         self.telefone= Entry(manutencao1,width=15)
+        # self.telefone.grid(row=4, column=1,sticky=W+E+N+S)
+         self.telefone.grid(row=4, column=1,sticky=W)
+         Label(manutencao1, text="Tipo:",font=('Arial', 15)).grid(row=5, column=0,sticky=W)
+         self.tipo = Entry(manutencao1,width=3)
+         self.tipo.grid(row=5, column=1,sticky=W)
+         Label(manutencao1, text="Cpf:",font=('Arial', 15)).grid(row=6, column=0,sticky=W)
+         self.cpf = Entry(manutencao1,width=16)
+         self.cpf.grid(row=6, column=1,sticky=W)
+         Label(manutencao1, text="Cnpj:",font=('Arial', 15)).grid(row=7, column=0,sticky=W)
+         self.cnpj = Entry(manutencao1,width=17)
+         self.cnpj.grid(row=7, column=1,sticky=W)
      
          manutencao1.geometry("%dx%d+%d+%d" % (1200, 650, centralizatela.posx,                    
          centralizatela.posy-20))
      
-         print (f'self.codigo.get = {self.codigo.get()}')    
-         #posx-200 ajuste de tela
-         
+             
 
 class  centralizacao():
    def __init__(self,manutencao,largura1, altura1,posx,posy):
@@ -72,16 +71,8 @@ class  centralizacao():
        # print(largura_screen, altura_screen)
       self.posx=largura_screen/2 - self.largura1/2
       self.posy= altura_screen/2 - self.altura1/2 
-      #janela1.geometry("%dx%d+%d+%d" % (largura, altura, posx, posy))
-      print(f'a posy = {posy} e a posx = {posx}')
-    
+      
+          
 
 
-      '''  for key, value in self.labelcria.items():
-              # globals()['strg%s' % n] = 'Hello'
-              globals()[value] = 'Hello'
-              print(f'{value} é igual = {codigo}')
-              #exec("%s = " % (value,""))
-              self.codigo="helo"
-        '''       
-   #exec("%s = %d" % (name,100))
+      
