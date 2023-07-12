c915f6c4c77a3de3bf486feb085d44dd15918a36
FRANCISCO CARLOS PIERNA <pierna_f@hotmail.com>
2023-07-12 15:40:36 -0300

    Tela Menu 12/07/2023

--- contasapagar/contaspagar.py
+++ contasapagar/contaspagar.py
@@ -14,39 +14,88 @@ ler=""
 opcao=0
 
 
-def verconteudo():
-     print (f'self.codigo.get = {tela.codigo.get()}') #{tela.codigo.get()}')
+#def verconteudo():
+#     print (f'self.codigo.get = {tela.codigo.get()}') #{tela.codigo.get()}')
      
-def incluir_click():
+def incluirfor_click():
      opcao=1
      global tela  
      manutencao = Toplevel() # janela de nível superior
      tela = montatela(manutencao,janela1,opcao,posx,posy)
-     botao=Button(manutencao, text='contem', command = verconteudo)
-     botao.grid(row=20, column=0,sticky=N)
      
-def cosulta_click():
+     
+def cosultafor_click():
      opcao=2
      global tela
      manutencao = Toplevel() # janela de nível superior
      tela = montatela(manutencao,janela1,opcao,posx,posy)
-     botao=Button(manutencao, text='contem', command = verconteudo)
-     botao.grid(row=20, column=0,sticky=E) 
-def alteracao_clik():
+def alteracaofor_clik():
      opcao=3
      global tela
      manutencao = Toplevel() # janela de nível superior
      tela = montatela(manutencao,janela1,opcao,posx,posy)
-     botao=Button(manutencao, text='contem', command = verconteudo)
-     botao.grid(row=20, column=0,sticky=N)
-def excluir_click(): 
+     
+def excluirfor_click(): 
      opcao=4
      global tela
      manutencao = Toplevel() # janela de nível superior
      tela = montatela(manutencao,janela1,opcao,posx,posy)
-     botao=Button(manutencao, text='contem', command = verconteudo)
-     botao.grid(row=20, column=1,sticky=N)
+    # botao=Button(manutencao, text='contem', command = verconteudo)
+    # botao.grid(row=20, column=1,sticky=N)
  
+
+ 
+
+def incluicontas_click():
+     opcao=1
+     global tela  
+     manutencao = Toplevel() # janela de nível superior
+     tela = montatela(manutencao,janela1,opcao,posx,posy)
+     
+def consultacontas_click():
+     opcao=2
+     global tela
+     manutencao = Toplevel() # janela de nível superior
+     tela = montatela(manutencao,janela1,opcao,posx,posy)
+ 
+def alteracaocontas_clik():
+     opcao=3
+     global tela
+     manutencao = Toplevel() # janela de nível superior
+     tela = montatela(manutencao,janela1,opcao,posx,posy)
+
+def exclusaocontas_click(): 
+     opcao=4
+     global tela
+     manutencao = Toplevel() # janela de nível superior
+     tela = montatela(manutencao,janela1,opcao,posx,posy)
+
+ 
+def incluirtipo_click():
+     opcao=1
+     global tela  
+     manutencao = Toplevel() # janela de nível superior
+     tela = montatela(manutencao,janela1,opcao,posx,posy)
+
+     
+def consultatipo_click():
+     opcao=2
+     global tela
+     manutencao = Toplevel() # janela de nível superior
+     tela = montatela(manutencao,janela1,opcao,posx,posy)
+
+def alteracaotipo_clik():
+     opcao=3
+     global tela
+     manutencao = Toplevel() # janela de nível superior
+     tela = montatela(manutencao,janela1,opcao,posx,posy)
+
+def excluirtipo_click(): 
+     opcao=4
+     global tela
+     manutencao = Toplevel() # janela de nível superior
+     tela = montatela(manutencao,janela1,opcao,posx,posy)
+
 janela= Tk()
 janela.title("janela principal")
 janela.configure(background='Gray')
@@ -68,24 +117,42 @@ janela1.grab_set()    # impede que click na janela principal sem fechar janela a
 #janela1.overrideredirect(True)  
 
 #adicionando menu
+
 menujan = Menu(janela1)
 filemenu= Menu(menujan, tearoff=0,)
-filemenu.add_command(label = "inclusão",command=incluir_click)
-filemenu.add_command(label = "consulta",command=cosulta_click)
-filemenu.add_command(label = "alteração",command=alteracao_clik)
-filemenu.add_command(label = "excluir", command=excluir_click )
-filemenu.add_separator()
-filemenu.add_command(label='Sair', command=quit)
+filemenu.add_command(label = " Inclusão",command=incluirfor_click)
+filemenu.add_command(label = " Consulta",command=cosultafor_click)
+filemenu.add_command(label = " Alteração",command=alteracaofor_clik)
+filemenu.add_command(label = " Excluir", command=excluirfor_click )
+#filemenu.add_separator()
+#filemenu.add_command(label='Sair', command=quit)
 
-menujan.add_cascade(label = "Manutenção", menu = filemenu)
+menujan.add_cascade(label = "Fornecedor", menu = filemenu)
+cadmenu = Menu(menujan, tearoff=0)
+cadmenu.add_command(label = " Inclusão",command=incluicontas_click)
+cadmenu.add_command(label = " Consulta", command=consultacontas_click)
+cadmenu.add_command(label = " Alteração",command=alteracaocontas_clik)
+cadmenu.add_command(label = " Exclusão",command=exclusaocontas_click)
+menujan.add_cascade(label = "Contas a pagar", menu = cadmenu)
+
+cadtipo = Menu(menujan, tearoff=0)
+cadtipo.add_command(label = " Inclusão",command=incluirtipo_click)
+cadtipo.add_command(label = " Consulta", command=consultatipo_click)
+cadtipo.add_command(label = " Alteração",command=alteracaotipo_clik)
+cadtipo.add_command(label = " Exclusão",command=excluirtipo_click)
+menujan.add_cascade(label = "Tipo de pagamento", menu = cadtipo)
+
 editmenu = Menu(menujan, tearoff=0)
 editmenu.add_command(label = "contas a pagar")
 editmenu.add_command(label = "contas a pagar por fornecedor")
 editmenu.add_command(label = "Contas a pagar por nome")
 editmenu.add_command(label = "Contas a pagar por CNPJ Ou CPF")
 menujan.add_cascade(label = "Relatórios", menu = editmenu)
+menusair = Menu(menujan, tearoff=0)
+menusair.add_command(label= "Sair click aqui", command=quit) 
+menujan.add_cascade(label='para Sair',menu = menusair)
 janela1.config(menu=menujan) #linha necessaria para aprecer o menu na janela de trabalho
-largura= 400
+largura= 500
 altura = 400
 centro=centralizacao(janela1,largura, altura, posx, posy)
 
